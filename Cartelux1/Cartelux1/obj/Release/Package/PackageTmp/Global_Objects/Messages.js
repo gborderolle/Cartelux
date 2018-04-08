40767403d238ec654ce8b8df4746cb0ff869d161
Gonzalo Borderolle <gborderolle@gmail.com>
2018-04-08 02:40:46 -0300

    as

--- Cartelux1/Cartelux1/obj/Release/Package/PackageTmp/Global_Objects/Messages.js
+++ Cartelux1/Cartelux1/obj/Release/Package/PackageTmp/Global_Objects/Messages.js
@@ -1,6 +1,6 @@
 ﻿
 var hashMessages = {};
 
-hashMessages["ConfirmacionPedido"] = "¡Ingreso correcto! Por favor notifíquelo a su vendedor.";
+hashMessages["ConfirmacionPedido"] = "¡MUCHAS GRACIAS!";
 hashMessages["Msj_inicioCliente"] = "Hola, soy de Cartelux. ¡Un gusto!";
 hashMessages["FaltanDatos"] = "Por favor, complete todos los datos";
